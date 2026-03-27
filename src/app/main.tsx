c6d010777f5037a1b206b0a6bcc7d128604e0feb
Jieco Lao <jiecoandres.lao07@gmail.com>
2026-03-28 00:21:32 +0800

    Updated Pages and Added Signup Page

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -6,6 +6,7 @@ import App from './App.tsx'
 import WatcherTestPage from './tests/backend-test.tsx'
 import LoginPage from './pages/LoginPage.tsx'
 import GamePage from './pages/GamePage.tsx'
+import SignupPage from './pages/SignupPage.tsx'
 
 /**
  *  
@@ -19,6 +20,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App />} />
         <Route path='/game' element={<GamePage/>}/>  
         <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/signup' element={<SignupPage/>}/>
         <Route path="/test" element={<WatcherTestPage />} />
       </Routes>
     </BrowserRouter>
--- src/app/pages/GamePage.tsx
+++ src/app/pages/GamePage.tsx
@@ -1,7 +1,6 @@
 import CodeEditor from "@/src/components/game-ui/CodeEditor"
 import { LootInventoryTree } from "@/src/components/game-ui/LootInventoryTree"
 import SideBar from "@/src/components/game-ui/SideBar"
-import { Group, Panel, Separator } from "react-resizable-panels"
 
 export default function GamePage() {
   return (
@@ -19,21 +18,8 @@ export default function GamePage() {
 
         <div className="relative flex h-full w-full border-2"> {/* scene */}
           <div className="">
-            <SideBar/>
+            <SideBar/> 
           </div>
-
-
-
-          {/* <Group orientation="horizontal" className="absolute top-0">
-            <Panel defaultSize={50} minSize={300} className="relative bg-amber-800">
-              <CodeEditor/>
-            </Panel>
-            <Separator className="w-0.5 bg-gray-600 hover:bg-gray-500 cursor-col-resize" />
-            <Panel minSize={685}>
-              <SideBar/>
-            </Panel>
-          </Group> */}
-
           <div className="absolute flex right-0 h-full border">
             <LootInventoryTree/>
           </div>

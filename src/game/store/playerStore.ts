62115dd10ca75575c1a878ea89d97f013ef44478
Jieco Lao <jiecoandres.lao07@gmail.com>
2026-04-06 11:48:24 +0800

    Modified Player State

--- src/game/store/playerStore.ts
+++ src/game/store/playerStore.ts
@@ -30,6 +30,8 @@ interface PlayerStoreProps {
   setUserId: (user_id: string) => void;
   setUsername: (username: string) => void;
   setPassword: (pass: string) => void;
+  setDamage: (amount: number) => void;
+  resetDamage: () => void;
   takeDamage: (amount: number) => void;
   setAtkSpeed: (amount: number) => void;
   gainHealth: (amount: number) => void;
@@ -37,8 +39,8 @@ interface PlayerStoreProps {
   gainEnergy: (amount: number) => void;
   loseEnergy: (amount: number) => void;
   toggleIsDamaged: () => void;
-  equipLeftHandWith: (weapon: string) => void;
-  equipRightHandWith: (weapon: string) => void;
+  equipLeftHandWith: (weapon: string, weaponDmg: number) => void;
+  equipRightHandWith: (weapon: string, weaponDmg: number) => void;
   unequipLeftHand: () => void;
   unequipRightHand: () => void;
   logOut: () => void;
@@ -66,6 +68,8 @@ export const usePlayerStore = create<PlayerStoreProps>((set) => ({
   setUserId: (user_id) => set(({ user_id: user_id })),
   setUsername: (username) => set(({ username: username })),
   setPassword: (pass) => set(({ password: pass })),
+  setDamage: (amount) => set((state) => ({ baseDmg: amount })),
+  resetDamage: () => set({ baseDmg: 2 }),
   takeDamage: (amount) => set((state) => ({ hp: Math.max(0, state.hp - amount) })),
   setAtkSpeed: (amount) => set((state) => ({ atkSpeed: amount })), // TODO: Configure
   gainHealth: (amount) => set((state) => ({ hp: state.hp + amount })),
@@ -73,10 +77,16 @@ export const usePlayerStore = create<PlayerStoreProps>((set) => ({
   gainEnergy: (amount) => set((state) => ({ energy: state.energy + amount})),
   loseEnergy: (amount) => set((state) => ({ energy: state.energy - amount })),
   toggleIsDamaged: () => set((state) => ({ isDamaged: !state.isDamaged })),
-  equipLeftHandWith: (weapon) => set({ leftHand: weapon }),
-  equipRightHandWith: (weapon) => set({ rightHand: weapon }),
-  unequipLeftHand: () => set({ leftHand: "" }),
-  unequipRightHand: () => set({ rightHand: "" }),
+  equipLeftHandWith: (weapon, weaponDmg) => set({ 
+    leftHand: weapon,
+    baseDmg: weaponDmg
+  }),
+  equipRightHandWith: (weapon, weaponDmg) => set({ 
+    rightHand: weapon,
+    baseDmg: weaponDmg 
+  }),
+  unequipLeftHand: () => set({ leftHand: "", baseDmg: 2 }),
+  unequipRightHand: () => set({ rightHand: "", baseDmg: 2 }),
   logOut: () => {
     useInventoryStore.setState({ player_id: "" })
     set({ 

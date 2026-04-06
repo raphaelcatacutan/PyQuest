2f3b72f13fd87916807092a3664ce9bd23422e12
Jieco Lao <jiecoandres.lao07@gmail.com>
2026-04-06 21:33:44 +0800

    Major Update to Player Store

--- src/game/store/playerStore.ts
+++ src/game/store/playerStore.ts
@@ -1,48 +1,58 @@
 import { create } from 'zustand'
 import { useInventoryStore } from './inventoryStore';
+import { Player } from '../types/player.types';
 
 /**
  * 
  *  Player State
  */
 
-// TODO: Plan how to store Inventory Tree attr
+// TODO: ToLocalStorage
 
-interface PlayerStoreProps {
-  user_id: string;
-  username: string;
-  password: string;
-  hp: number;
-  maxHP: number;
-  def: number;
-  maxDef: number;
-  energy: number;
-  maxEnergy: number;
-  level: number;
-  XP: number;
-  maxXP: number;
-  isDamaged: boolean;
-  leftHand: string;
-  rightHand: string;
-  baseDmg: number;
-  baseCritChance: number;
-  atkSpeed: number;
+interface PlayerStoreProps extends Player {
   setUserId: (user_id: string) => void;
   setUsername: (username: string) => void;
   setPassword: (pass: string) => void;
-  setDamage: (amount: number) => void;
-  resetDamage: () => void;
-  takeDamage: (amount: number) => void;
-  setAtkSpeed: (amount: number) => void;
-  gainHealth: (amount: number) => void;
-  gainXP: (amount: number) => void;
+
+  gainHP: (amount: number) => void;
+  setMaxHP: (amount: number) => void;
+  resetMaxHP: () => void
+
+  gainDef: (amount: number) => void;
+  setMaxDef: (amount: number) => void;
+  resetMaxDef: (amount: number) => void;
+
   gainEnergy: (amount: number) => void;
-  loseEnergy: (amount: number) => void;
-  toggleIsDamaged: () => void;
+  setMaxEnergy: (amount: number) => void;
+  spendEnergy: (amount: number) => void;
+  resetMaxEnergy: () => void;
+
+  setDamage: (amount: number) => void;
+  takeDamage: (amount: number) => void;
+  resetDamage: () => void;
+
+  setCritDmg: (amount: number) => void;
+  resetCritDmg: () => void;
+  setCritChance: (amount: number) => void;
+  resetCritChange: () => void;
+
+  setAtkSpeed: (amount: number) => void;
+  resetAtkSpeed: () => void;
+
   equipLeftHandWith: (weapon: string, weaponDmg: number) => void;
   equipRightHandWith: (weapon: string, weaponDmg: number) => void;
   unequipLeftHand: () => void;
   unequipRightHand: () => void;
+  equipHeadSlotWith: (headArmor: string, defBonus: number) => void;
+  equipBodySlotWith: (bodyArmor: string, defBonus: number) => void;
+  unequipHeadArmor: () => void;
+  unequipBodyArmor: () => void;
+
+  gainXP: (amount: number) => void;
+  setXpRequirement: (amount: number) => void;
+  levelUp: () => void;
+
+  toggleIsDamaged: () => void;
   logOut: () => void;
 }
 
@@ -50,43 +60,71 @@ export const usePlayerStore = create<PlayerStoreProps>((set) => ({
   user_id: "",
   username: "",
   password: "",
+
   hp: 0,
   maxHP: 100,
   def: 0,
   maxDef: 0,
-  energy: 0,
+  energy: 100,
   maxEnergy: 100,
-  level: 0,
-  XP: 0,
   maxXP: 0,
-  isDamaged: false,
-  leftHand: "",
-  rightHand: "",
   baseDmg: 2,
+  baseCritDmg: 0,
   baseCritChance: 3, 
   atkSpeed: 3000,
+  
+  leftHand: "",
+  rightHand: "",
+  headSlot: "",
+  bodySlot: "",
+  
+  XP: 0,
+  xpRequirement: 100,
+  level: 1,
+  isDamaged: false,
   setUserId: (user_id) => set(({ user_id: user_id })),
   setUsername: (username) => set(({ username: username })),
   setPassword: (pass) => set(({ password: pass })),
-  setDamage: (amount) => set((state) => ({ baseDmg: amount })),
-  resetDamage: () => set({ baseDmg: 2 }),
-  takeDamage: (amount) => set((state) => ({ hp: Math.max(0, state.hp - amount) })),
-  setAtkSpeed: (amount) => set((state) => ({ atkSpeed: amount })), // TODO: Configure
-  gainHealth: (amount) => set((state) => ({ hp: state.hp + amount })),
-  gainXP: (amount) => set((state) => ({ XP: state.XP + amount })),
+
+  gainHP: (amount) => set((state) => ({ hp: state.hp + amount })),  
+  setMaxHP: (amount) => set((state) => ({ maxHP: state.maxHP + amount })),
+  resetMaxHP: () => set({ maxHP: 100 }),
+
+  gainDef: (amount) => set((state) => ({ def: state.def + amount })),
+  setMaxDef: (amount) => set((state) => ({ maxDef: state.maxDef + amount })),
+  resetMaxDef: (amount) => set({ def: 0, maxDef: 0 }),
+
   gainEnergy: (amount) => set((state) => ({ energy: state.energy + amount})),
-  loseEnergy: (amount) => set((state) => ({ energy: state.energy - amount })),
-  toggleIsDamaged: () => set((state) => ({ isDamaged: !state.isDamaged })),
-  equipLeftHandWith: (weapon, weaponDmg) => set({ 
-    leftHand: weapon,
-    baseDmg: weaponDmg
-  }),
-  equipRightHandWith: (weapon, weaponDmg) => set({ 
-    rightHand: weapon,
-    baseDmg: weaponDmg 
-  }),
+  setMaxEnergy: (amount) => set((state) => ({ maxEnergy: state.maxEnergy + amount })),
+  spendEnergy: (amount) => set((state) => ({ energy: state.energy - amount })),
+  resetMaxEnergy: () => set({ maxEnergy: 100 }),
+
+  setDamage: (amount) => set((state) => ({ baseDmg: state.baseDmg + amount })),
+  takeDamage: (amount) => set((state) => ({ hp: Math.max(0, state.hp - amount) })),
+  resetDamage: () => set({ baseDmg: 2 }),
+
+  setCritDmg: (amount) => set((state) => ({ baseCritDmg: state.baseCritDmg + amount })),
+  resetCritDmg: () => set({ baseCritDmg: 0 }),
+  setCritChance: (amount) => set((state) => ({ baseCritChance: state.baseCritChance + amount })),
+  resetCritChange: () => set({ baseCritChance: 0 }),
+
+  setAtkSpeed: (amount) => set((state) => ({ atkSpeed: state.atkSpeed + amount })),
+  resetAtkSpeed: () => set({ atkSpeed: 3000 }),
+  
+  equipLeftHandWith: (weapon, weaponDmg) => set((state) => ({ leftHand: weapon, baseDmg: state.baseDmg + weaponDmg })),
+  equipRightHandWith: (weapon, weaponDmg) => set((state) => ({ rightHand: weapon, baseDmg: state.baseDmg + weaponDmg })),
   unequipLeftHand: () => set({ leftHand: "", baseDmg: 2 }),
   unequipRightHand: () => set({ rightHand: "", baseDmg: 2 }),
+  equipHeadSlotWith: (headArmor, defBonus) => set((state) => ({ headSlot: headArmor, def: state.def + defBonus })),
+  equipBodySlotWith: (bodyArmor, defBonus) => set((state) => ({ bodySlot: bodyArmor, def: state.def + defBonus })),
+  unequipHeadArmor: () => set({ def: 0 }), // TODO: Figure out how to reduce a certain amount of def provided by the helmet
+  unequipBodyArmor: () => set({ def: 0 }), // TODO: Figure out how to reduce a certain amount of def provided by the helmet
+
+  gainXP: (amount) => set((state) => ({ XP: state.XP + amount })),
+  setXpRequirement: (amount) => set({ xpRequirement: amount }),
+  levelUp: () => set((state) => ({ level: state.level + 1 })),
+
+  toggleIsDamaged: () => set((state) => ({ isDamaged: !state.isDamaged })),
   logOut: () => {
     useInventoryStore.setState({ player_id: "" })
     set({ 
---
+++ src/game/types/player.types.ts
@@ -0,0 +1,27 @@
+export interface Player {  
+  user_id: string;
+  username: string;
+  password: string;
+
+  hp: number;
+  maxHP: number;
+  def: number;
+  maxDef: number;
+  energy: number;
+  maxEnergy: number;
+  maxXP: number;
+  baseDmg: number;
+  baseCritDmg: number,
+  baseCritChance: number;
+  atkSpeed: number;
+  
+  leftHand: string;
+  rightHand: string;
+  headSlot: string;
+  bodySlot: string;
+  
+  XP: number;
+  xpRequirement: number;
+  level: number;
+  isDamaged: boolean;
+}

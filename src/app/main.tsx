1d432e3dbdfdf64fc7c4317587ff3377ef3953d6
raphaelcatacutan <raphaelcatacutan@gmail.com>
2026-04-15 18:53:37 +0800

    Removed backend test

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './styles/global.css'
 import App from './App.tsx'
-import WatcherTestPage from './tests/backend-test.tsx'
 import LoginPage from './pages/LoginPage.tsx'
 import GamePage from './pages/GamePage.tsx'
 import SignupPage from './pages/SignupPage.tsx'
@@ -27,7 +26,6 @@ createRoot(document.getElementById('root')!).render(
           <Route path='/game' element={<GamePage/>}/>  
           <Route path='/login' element={<LoginPage/>}/>
           <Route path='/signup' element={<SignupPage/>}/>
-          <Route path="/test" element={<WatcherTestPage />} />
           <Route path="/architect" element={<UnifiedArchitect/>}/>
         </Routes>
       </BrowserRouter>
